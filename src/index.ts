985b74198ab8f60c14653e4f669e27b3703e4aa0
Neelesh <neeleshrana22@gmail.com>
2025-05-28 14:09:53 +0530

    signup route

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,44 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.post('/api/v1/signup',(c)=>{
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
+}>();
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password,
+    },
+  })
   return c.text('Signup endpoint')
 })
 
-app.post('/api/v1/signin',(c)=>{
+app.post('/api/v1/signin', (c) => {
   return c.text('Signin endpoint')
 })
 
-app.post('/api/v1/blog',(c)=>{
+app.post('/api/v1/blog', (c) => {
   return c.text('Blog endpoint')
 })
 
-app.put('/api/v1/blog/:id',(c)=>{
+app.put('/api/v1/blog/:id', (c) => {
   return c.text(`Update blog with ID: ${c.req.param('id')}`)
 })
 
-app.get('/api/v1/blog/:id',(c)=>{
+app.get('/api/v1/blog/:id', (c) => {
   return c.text(`Get blog with ID: ${c.req.param('id')}`)
 })
 
